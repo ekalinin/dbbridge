bfac3d6a0e8ea284b95763ddeb59824e24b571c6
Eugene Kalinin <e.v.kalinin@gmail.com>
2026-06-12 00:43:06 +0300

    chore: fix deprecated middlewar

--- internal/transport/rest/server.go
+++ internal/transport/rest/server.go
@@ -41,7 +41,7 @@ func (s *Server) Handler() http.Handler {
 
 func (s *Server) setupRoutes() {
 	s.router.Use(middleware.RequestID)
-	s.router.Use(middleware.RealIP)
+	s.router.Use(middleware.ClientIPFromXFF())
 	s.router.Use(middleware.Logger)
 	s.router.Use(middleware.Recoverer)
 	s.router.Use(middleware.Timeout(60 * time.Second))
